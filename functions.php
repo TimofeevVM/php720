2ac149606a658b28e59fc96f2b9da1932a396d82
Vlad Timofeev <TimofeevVM1@gmail.com>
2019-01-23 15:32:24 +0500

    Task 8

--- functions.php
+++ functions.php
@@ -13,13 +13,14 @@
      *
      * @return int|null
      */
-    function sumDigitalsOfNumber($number){
-        if (!is_numeric($number)){
+    function sumDigitalsOfNumber($number)
+    {
+        if (!is_numeric($number)) {
             return null;
         }
 
         $sum = 0;
-        for ($i=0; $i < strlen($number); $i++){
+        for ($i = 0; $i < strlen($number); $i++) {
             $sum += $number[$i];
         }
 
@@ -33,12 +34,13 @@
      *
      * @return int
      */
-    function countDigitInNumber($number,$digit){
+    function countDigitInNumber($number, $digit)
+    {
         $count = 0;
         $number = strval($number);
 
-        for ($i=0; $i < strlen($number); $i++){
-            if ($number[$i] == $digit){
+        for ($i = 0; $i < strlen($number); $i++) {
+            if ($number[$i] == $digit) {
                 $count++;
             }
         }
@@ -51,15 +53,16 @@
      * @return int
      *
      */
-    function sumFmod5(){
+    function sumFmod5()
+    {
         $start = 20;
         $end = 45;
         $divider = 5;
 
         $sum = 0;
 
-        for ($i = $start; $i <= $end; $i++){
-            if (fmod($i,$divider)==0){
+        for ($i = $start; $i <= $end; $i++) {
+            if (fmod($i, $divider) == 0) {
                 $sum += $i;
             }
         }
@@ -73,18 +76,19 @@
      *
      * @return array
      */
-    function replaceMaxAndMinNumber(){
+    function replaceMaxAndMinNumber()
+    {
         $arr = [];
-        for ($i = 0; $i<10; $i++){
-            $arr[] = rand(0,20);
+        for ($i = 0; $i < 10; $i++) {
+            $arr[] = rand(0, 20);
         }
         $orgArr = $arr; // для вывода
-        $maxKey = array_keys($arr,max($arr))[0];
-        $minKey = array_keys($arr,min($arr))[0];
+        $maxKey = array_keys($arr, max($arr))[0];
+        $minKey = array_keys($arr, min($arr))[0];
 
-        list($arr[$maxKey],$arr[$minKey]) = [$arr[$minKey],$arr[$maxKey]];
+        list($arr[$maxKey], $arr[$minKey]) = [$arr[$minKey], $arr[$maxKey]];
 
-        return ['orgArr'=>$orgArr,'arr'=>$arr];
+        return ['orgArr' => $orgArr, 'arr' => $arr];
     }
 
     /**
@@ -94,23 +98,24 @@
      *
      * @return array
      */
-    function replaceMaxAndMinNumber1(){
+    function replaceMaxAndMinNumber1()
+    {
         $arr = [];
         $lenArr = 10;
 
-        for ($i = 0; $i<$lenArr; $i++){
-            $arr[] = rand(0,20);
+        for ($i = 0; $i < $lenArr; $i++) {
+            $arr[] = rand(0, 20);
         }
         $orgArr = $arr; // для вывода
         $maxKey = 0;
         $minKey = 0;
 
-        for ($i = 0; $i<$lenArr; $i++){
-            if ($arr[$minKey] < $arr[$i]){
+        for ($i = 0; $i < $lenArr; $i++) {
+            if ($arr[$minKey] < $arr[$i]) {
                 $minKey = $i;
             }
 
-            if ($arr[$maxKey] > $arr[$i]){
+            if ($arr[$maxKey] > $arr[$i]) {
                 $maxKey = $i;
             }
         }
@@ -119,27 +124,28 @@
         $arr[$maxKey] = $arr[$minKey];
         $arr[$minKey] = $buf;
 
-        return ['orgArr'=>$orgArr,'arr'=>$arr];
+        return ['orgArr' => $orgArr, 'arr' => $arr];
     }
 
     /**
      * Работа с индексами, элементами массива
      * @return array
      */
-    function workWithIndex(){
+    function workWithIndex()
+    {
         $arr = [];
         $lenArr = 10;
 
-        for ($i = 0; $i<$lenArr; $i++){
-            $arr[] = rand(1,100);
+        for ($i = 0; $i < $lenArr; $i++) {
+            $arr[] = rand(1, 100);
         }
 
-        $resArr=[];
+        $resArr = [];
         $op = 1;
 
-        for ($i = 0; $i<$lenArr; $i++){
-            if ($arr[$i]>0){
-                if (fmod($i,2) == 0){
+        for ($i = 0; $i < $lenArr; $i++) {
+            if ($arr[$i] > 0) {
+                if (fmod($i, 2) == 0) {
                     $op *= $arr[$i];
                 } else {
                     $resArr[] = $arr[$i];
@@ -147,7 +153,7 @@
             }
         }
 
-        return ['op'=>$op,'orgArr'=>$arr,'arr'=>$resArr];
+        return ['op' => $op, 'orgArr' => $arr, 'arr' => $resArr];
     }
 
     /**
@@ -157,10 +163,12 @@
      *
      * @return string
      */
-    function reductionFullName($fullName){
-        $arrName = explode(' ',$fullName);
-        return $arrName[0].' '.$arrName[1][0].'. '.$arrName[2][0];
+    function reductionFullName($fullName)
+    {
+        $arrName = explode(' ', $fullName);
+        return $arrName[0] . ' ' . $arrName[1][0] . '. ' . $arrName[2][0];
     }
+
 
     /**
      * Определения положения стрелки часов
@@ -169,14 +177,35 @@
      *
      * @return float|int|null
      */
-    function howHour($deg){
+    function howHour($deg)
+    {
         if ($deg < 0 || $deg > 360) return null;
-        $pct = round(12/360,2);
+        $pct = round(12 / 360, 2);
 
-        $hour = $pct*$deg;
-        return round($hour)+1;
+        $hour = $pct * $deg;
+        return round($hour) + 1;
     }
 
+    /**
+     * Рассчитать цвет сигнала светофора
+     *
+     * @param $n
+     *
+     * @return string|null
+     */
+    function trafficLight($n)
+    {
+        $green = [1, 2, 3];
+        $red = [0, 4];
+
+        $n = fmod($n, 5);
+        if (in_array($n, $green)) {
+            return "Green";
+        } elseif (in_array($n, $red)) {
+            return "Red";
+        }
+        return null;
+    }
 
     /**
      * Проверить високосный ли год
@@ -188,7 +217,7 @@
     function isLeapYear($year)
     {
         if (!($year > 0 && $year < 10000)) return null;
-        $y = DateTime::createFromFormat('Y',$year)->format('L');
+        $y = DateTime::createFromFormat('Y', $year)->format('L');
         return (boolean)$y;
     }
 
@@ -217,11 +246,9 @@
             '10' => 'Десятка',
         ];
 
-        if (array_key_exists($id,$cards))
-        {
+        if (array_key_exists($id, $cards)) {
             return $cards[$id];
-        } else
-        {
+        } else {
             return null;
         }
 
@@ -234,25 +261,26 @@
      *
      * @return string|null
      */
-    function chineseCalendar($year){
+    function chineseCalendar($year)
+    {
         $start = 1924;
         if ($year < $start) return null;
-        $animal=[
-          'крысы',
-          'быка',
-          'тигра',
-          'кролика',
-          'дракона',
-          'змеи',
-          'лошади',
-          'овцы',
-          'обезьяны',
-          'петуха',
-          'собака',
-          'свиньи',
+        $animal = [
+            'крысы',
+            'быка',
+            'тигра',
+            'кролика',
+            'дракона',
+            'змеи',
+            'лошади',
+            'овцы',
+            'обезьяны',
+            'петуха',
+            'собака',
+            'свиньи',
         ];
 
-        return "Год ".$animal[intval(fmod( $year-$start,12))];
+        return "Год " . $animal[intval(fmod($year - $start, 12))];
     }
 
     /**
@@ -262,12 +290,11 @@
      *
      * @return int
      */
-    function square($n){
+    function square($n)
+    {
         if ($n > 0) {
-            return square($n-1)+pow($n,$n);
-        }
-        else
-        {
+            return square($n - 1) + pow($n, $n);
+        } else {
             return $n;
         }
     }
